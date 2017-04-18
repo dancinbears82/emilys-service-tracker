9d606cd0ed1f38008f7a9eb0a535019db616580d
dancinbears82 <dancinbears82@yahoo.com>
2017-04-18 18:44:23 -0400

    CSS changes for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,47 @@
+import { WelcomeComponent } from './welcome/welcome.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
+
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
 
 const firebaseDatabaseConfig = {
-    apiKey: "AIzaSyBp7dpH9bcC4cSEUakIBq2FqBPcP_pe0zQ",
-    authDomain: "emilys-service-tracker.firebaseapp.com",
-    databaseURL: "https://emilys-service-tracker.firebaseio.com",
-    storageBucket: "emilys-service-tracker.appspot.com",
-    messagingSenderId: "41467514844"
+  apiKey: "AIzaSyBp7dpH9bcC4cSEUakIBq2FqBPcP_pe0zQ",
+  authDomain: "emilys-service-tracker.firebaseapp.com",
+  databaseURL: "https://emilys-service-tracker.firebaseio.com",
+  storageBucket: "emilys-service-tracker.appspot.com",
+  messagingSenderId: "41467514844"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
 };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
+    Ng2Bs3ModalModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(firebaseDatabaseConfig, firebaseAuthConfig),
+    HttpModule,
+    RouterModule.forRoot([
+
+      { path: 'login', component: LoginComponent },
+
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
